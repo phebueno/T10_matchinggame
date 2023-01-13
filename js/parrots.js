e28b98eca828135dd5784fa8e2f620216723379c
Phebueno <fcbgomes@gmail.com>
2023-01-12 22:24:12 -0300

    versão com click funcional completo

--- js/parrots.js
+++ js/parrots.js
@@ -2,7 +2,46 @@
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+// Função para desabilitar temporariamente a página
+function handler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    }
+// Seleção de carta
+function selecionaCarta(carta){
+    carta.classList.add('front-face');
+    const carta_virada = carta.parentNode.querySelector(':nth-child(2)'); //passa pelo elemento pai pra identificar o irmão da carta
+    carta_virada.classList.add('back-face');
+    if (img_carta==''){
+        img_carta = carta_virada.querySelector('img');
+    }
+    else{
+        //Só entra aqui na segunda escolha de carta
+        if (img_carta.src==carta_virada.querySelector('img').src){
+            img_carta=''; //Acertou!
+        }
+        else{
+            //Desabilita os cliques
+            document.addEventListener("click", handler, true);
+            setTimeout(resetar,1000,carta_virada.querySelector('img'));//Errou, vai resetar a dupla
+        }        
+    }
+}
+// Função para identificar a tentativa
+function resetar(nova_carta){            
+    img_carta.parentNode.classList.remove('back-face');
+    nova_carta.parentNode.classList.remove('back-face');
+    img_carta.parentNode.classList.add('escondido');
+    nova_carta.parentNode.classList.add('escondido');
+    img_carta.parentNode.parentNode.querySelector(':nth-child(1)').classList.remove('front-face');
+    nova_carta.parentNode.parentNode.querySelector(':nth-child(1)').classList.remove('front-face');
+    img_carta='';
+    //Reabilita os cliques
+    document.removeEventListener("click", handler, true); 
+}
 
+//Montagem do site e variáveis globais
+let img_carta = '';
 let check = false;
 let qtd_cartas = 0;
 let imagens = ['bobross','explody','fiesta','metal','revertit','triplets','unicorn'];
@@ -26,10 +65,10 @@ let cartas = document.querySelector('main');
 //Espalha as cartas no main, na ordem do array definido
 for (let i = 0; i < qtd_cartas; i++) {
     cartas.innerHTML+=`<div class="card">
-                            <div class="front-face face">
+                            <div class="face" onclick="selecionaCarta(this)">
                                 <img src="imgs/back.png" alt="">
                             </div>
-                            <div class="back-face face">
+                            <div class="escondido face">
                                 <img src="imgs/${baralho[i]}parrot.gif" alt="">
                             </div>
                         </div>`;
